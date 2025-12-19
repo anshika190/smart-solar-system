b3346f883fe321b79de96b81e9bfc4a12274489c
Anshika Garg <garganshika005@gmail.com>
2025-12-19 20:34:26 +0530

    CONFIG: Set base to relative paths v17.0

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()], // FORCE REBUILD TIMESTAMP 2025-12-19-FINAL-V13
+  plugins: [react()],
+  base: './', // Force relative paths v17.0
 })
